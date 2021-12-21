311025052e16ce9260d61946976c3bd15977dbc6
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2021-12-21 18:08:33 +0000

    worked out bug on previous render to html

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-let element = document.getElementById("id")
-let btn = document.getElementById("btn")
+/*==================== GENERATE PDF ====================*/ 
+// PDF generated area
 
-function createPDF () {
-    html2pdf(element)
+let areaCv = document.getElementById('id')
+
+let resumeButton = document.getElementById("resume-button")
+
+
+// Html2pdf options
+
+
+// Function to call areaCv and Html2Pdf options 
+function generateResume() {
+    html2pdf(areaCv)
 }
 
-btn.addEventListener("click", () => {
-    createPDF();
+// When the button is clicked, it executes the three functions
+resumeButton.addEventListener('click', () => {
+
+
+    // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+    // scaleCV()
+
+    // 2. The PDF is generated
+    generateResume()
+
+    // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
 })
