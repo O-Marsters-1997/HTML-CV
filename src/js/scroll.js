47c7e12104bbc1201d51f8b8d9e4f92e3126c83a
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2021-12-24 21:58:25 +0000

    refactor scroll functionality

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -8,27 +8,6 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-// /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-
-// const sections = document.querySelectorAll('section')
-
-// function scrollActive(){
-//     const scrollY = window.pageYOffset
-
-//     sections.forEach(current =>{
-//         const sectionHeight = current.offsetHeight
-//         const sectionTop = current.offsetTop - 50;
-//         sectionId = current.getAttribute('id')
-
-//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-//             document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
-//         }else{
-//             document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
-//         }
-//     })
-// }
-// window.addEventListener('scroll', scrollActive)
-
 /*==================== SHOW SCROLL TOP ====================*/
 
 // function scrollTop() {
