3d9d0bbd7f135d040462ffc5ec68e702a3684c74
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2021-12-26 12:41:27 +0000

    set up multiplier for font-sizes

--- src/js/main.js
+++ src/js/main.js
@@ -89,7 +89,7 @@ resumeButton.addEventListener('click', () => {
     // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
     // scaleCV()
     // 2. The PDF is generated
-    generateResume()
+    // generateResume()
     // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
 })
 
